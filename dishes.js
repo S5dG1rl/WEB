cd306727f735407f3e0df5c70f5932b9f5d997b1
S5dG1rl <91469855+S5dG1rl@users.noreply.github.com>
2025-12-18 14:42:31 +0300

    Create dishes.js

---
+++ dishes.js
@@ -0,0 +1,17 @@
+// dishes.js
+const dishes = [
+  // Супы
+  { keyword: "borsh", name: "Борщ", price: 210, category: "soup", count: "300 мл", image: "images/борщ.jpg" },
+  { keyword: "tom_yam", name: "Том Ям", price: 240, category: "soup", count: "300 мл", image: "images/том.jpg" },
+  { keyword: "chicken_soup", name: "Куриный суп с лапшой", price: 180, category: "soup", count: "300 мл", image: "images/кур.jpg" },
+
+  // Главные блюда
+  { keyword: "lasagna", name: "Лазанья", price: 385, category: "main", count: "400 г", image: "images/лаз.jpg" },
+  { keyword: "beef_stroganoff", name: "Бефстроганов", price: 340, category: "main", count: "380 г", image: "images/беф.jpg" },
+  { keyword: "salmon", name: "Сёмга на гриле", price: 390, category: "main", count: "350 г", image: "images/сем.jpg" },
+
+  // Напитки
+  { keyword: "lemonade", name: "Домашний лимонад", price: 120, category: "drink", count: "400 мл", image: "images/лим.jpg" },
+  { keyword: "green_tea", name: "Зелёный чай", price: 90, category: "drink", count: "400 мл", image: "images/зел.jpg" },
+  { keyword: "americano", name: "Американо", price: 150, category: "drink", count: "300 мл", image: "images/кофе.jpg" }
+];
